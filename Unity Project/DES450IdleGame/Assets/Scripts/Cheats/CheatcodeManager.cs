9d11144dab19a48ab5351bfc588e46c9272bdf48
BarBenzvi <bar.ben-zvi@digipen.edu>
2023-01-29 22:49:39 -0800

    Added basic cheat code manager

---
+++ Unity Project/DES450IdleGame/Assets/Scripts/Cheats/CheatcodeManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheatcodeManager : MonoBehaviour
+{
+    public bool CheatsActive = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!CheatsActive)
+        {
+            return;
+        }
+
+        if(Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            if(Input.GetKeyUp(KeyCode.Alpha1))
+            {
+                GlobalGameData.Coins += new BigNumber(1, 2);
+            }
+            if (Input.GetKeyUp(KeyCode.Alpha2))
+            {
+                GlobalGameData.Coins += new BigNumber(1, 3);
+            }
+            if (Input.GetKeyUp(KeyCode.Alpha3))
+            {
+                GlobalGameData.Coins += new BigNumber(1, 4);
+            }
+            if (Input.GetKeyUp(KeyCode.Alpha4))
+            {
+                GlobalGameData.Coins += new BigNumber(1, 10);
+            }
+
+            if(Input.GetKeyUp(KeyCode.D))
+            {
+                BigNumber.DisplayType d = BigNumber.GetDisplayType();
+                if(d == BigNumber.DisplayType.NAMES)
+                {
+                    BigNumber.SetDisplayType(BigNumber.DisplayType.SCIENTIFIC);
+                }
+                else
+                {
+                    BigNumber.SetDisplayType(BigNumber.DisplayType.NAMES);
+                }
+            }
+        }
+    }
+}
