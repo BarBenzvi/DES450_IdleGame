908cd7a4c34e490a5facdfcd79f8800e88a2245b
BarBenzvi <bar.ben-zvi@digipen.edu>
2023-02-22 21:13:59 -0800

    Fixed healthbars, setup cursor upgrade text

--- Unity Project/DES450IdleGame/Assets/Scripts/Upgrades/CursorUpgrader.cs
+++ Unity Project/DES450IdleGame/Assets/Scripts/Upgrades/CursorUpgrader.cs
@@ -1,15 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CursorUpgrader : UpgradeButton
 {
     public int LinearClickIncrease = 1;
 
+    public TextMeshProUGUI DamageText = null;
+    public TextMeshProUGUI IncomeText = null;
+
+    CursorBehavior c = null;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        c = GameObject.Find("Cursor").GetComponent<CursorBehavior>();
+    }
+
     protected override void UpgradeEffects()
     {
-        CursorBehavior c = GameObject.Find("Cursor").GetComponent<CursorBehavior>();
         c.DamagePerClick += LinearClickIncrease;
         c.CoinsPerClick += LinearClickIncrease;
+    }
+
+    protected override void UpdateText()
+    {
+        base.UpdateText();
+
+        if(DamageText)
+        {
+            DamageText.text = "Damage: " + c.DamagePerClick.ToString();
+        }
+        if (IncomeText)
+        {
+            IncomeText.text = "Click Income: " + c.CoinsPerClick.ToString();
+        }
     }
 }
--- Unity Project/DES450IdleGame/Assets/Scripts/Upgrades/UpgradeButton.cs
+++ Unity Project/DES450IdleGame/Assets/Scripts/Upgrades/UpgradeButton.cs
@@ -8,18 +8,36 @@ public class UpgradeButton : MonoBehaviour
     public BigNumber BaseCost = new BigNumber(2.5f, 2);
     public float CostMultiplier = 1.5f;
 
+    public string LevelPreText = "";
+
     public TextMeshProUGUI CostText = null;
+    public TextMeshProUGUI LevelText = null;
 
     BigNumber currCost;
+    
+    protected int timesPurchased = 0;
 
-    private void Start()
+    protected virtual void Start()
     {
         currCost = new BigNumber(BaseCost);
+        timesPurchased = 0;
     }
 
     private void Update()
     {
-        CostText.text = currCost.ToString() + " Coins";
+        UpdateText();
+    }
+
+    protected virtual void UpdateText()
+    {
+        if(CostText)
+        {
+            CostText.text = currCost.ToString() + " Coins";
+        }
+        if(LevelText)
+        {
+            LevelText.text = LevelPreText + (timesPurchased + 1).ToString();
+        }
     }
 
     virtual protected void UpgradeEffects()
@@ -32,6 +50,7 @@ public class UpgradeButton : MonoBehaviour
             GlobalGameData.Coins -= currCost;
 
             currCost *= CostMultiplier;
+            timesPurchased += 1;
 
             UpgradeEffects();
         }
