4a0c54930949d3dbddf863a2fcd0b8e5e7b40b63
BarBenzvi <bar.ben-zvi@digipen.edu>
2023-02-22 21:24:50 -0800

    Monster Upgrade Text

--- Unity Project/DES450IdleGame/Assets/Scripts/Upgrades/MonsterUpgrader.cs
+++ Unity Project/DES450IdleGame/Assets/Scripts/Upgrades/MonsterUpgrader.cs
@@ -1,16 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MonsterUpgrader : UpgradeButton
 {
     public float DamageMultiplier = 1.0f;
     public float AttackSpeedMultiplier = 1.0f;
 
+    public TextMeshProUGUI DamageText = null;
+    public TextMeshProUGUI AttackSpeedText = null;
+
+    MonsterBehavior m = null;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        m = GameObject.Find("SeaMonster").GetComponent<MonsterBehavior>();
+    }
+
     protected override void UpgradeEffects()
     {
-        MonsterBehavior m = GameObject.Find("SeaMonster").GetComponent<MonsterBehavior>();
         m.BaseDamage *= DamageMultiplier;
         m.AttackCooldown *= AttackSpeedMultiplier;
+    }
+
+    protected override void UpdateText()
+    {
+        base.UpdateText();
+
+        if(DamageText)
+        {
+            DamageText.text = "Damage: " + m.BaseDamage.ToString();
+        }
+        if(AttackSpeedText)
+        {
+            AttackSpeedText.text = "Attack Speed: " + string.Format("{0:F1}", 1.0f / m.AttackCooldown) + "/s";
+        }
     }
 }
