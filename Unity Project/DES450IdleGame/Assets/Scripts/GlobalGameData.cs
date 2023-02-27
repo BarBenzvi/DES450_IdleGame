beec6b4261104a5f2437426558e3a341149c81dc
BarBenzvi <bar.ben-zvi@digipen.edu>
2023-02-26 17:03:23 -0800

    leveling monster generates points and platinum

--- Unity Project/DES450IdleGame/Assets/Scripts/GlobalGameData.cs
+++ Unity Project/DES450IdleGame/Assets/Scripts/GlobalGameData.cs
@@ -8,7 +8,9 @@ public abstract class GlobalGameData
     public static BigNumber Coins = new BigNumber(0, 0);
 
     // Global variable for the player's current amount of platinum
-    public static BigNumber Platinum = new BigNumber(1, 0);
+    public static BigNumber Platinum = new BigNumber(0, 0);
+
+    public static int SkillTreePoints = 0;
 
     public static BigNumber BoatIncomeMultiplier = new BigNumber(1, 0);
     public static BigNumber BoatHealthMultiplier = new BigNumber(1, 0);
--- Unity Project/DES450IdleGame/Assets/Scripts/Upgrades/MonsterUpgrader.cs
+++ Unity Project/DES450IdleGame/Assets/Scripts/Upgrades/MonsterUpgrader.cs
@@ -8,6 +8,9 @@ public class MonsterUpgrader : UpgradeButton
     public float DamageMultiplier = 1.0f;
     public float AttackSpeedMultiplier = 1.0f;
 
+    public int PlatinumLevelInterval = 1;
+    public int SkillPointLevelInterval = 10;
+
     public TextMeshProUGUI DamageText = null;
     public TextMeshProUGUI AttackSpeedText = null;
 
@@ -25,6 +28,14 @@ public class MonsterUpgrader : UpgradeButton
         m.BaseDamage *= DamageMultiplier;
         m.AttackCooldown *= AttackSpeedMultiplier;
         m.Level += 1;
+        if(m.Level % PlatinumLevelInterval == 0)
+        {
+            GlobalGameData.Platinum += 1;
+        }
+        if(m.Level % SkillPointLevelInterval == 0)
+        {
+            GlobalGameData.SkillTreePoints += 1;
+        }
     }
 
     protected override void UpdateText()
